72547e00e88b83fea32a398148d68a2afd4dad97
Shyam Sundar N R <nrshyamsundar@gmail.com>
2023-03-30 11:34:58 +0530

    first run

--- main.cpp
+++ main.cpp
@@ -1,30 +1,29 @@
 #include<bits/stdc++.h>
+#include<chrono>
 
 #include"mmh.h"
 #include"sqh.h"
 
-// #include<chrono>
 
-// using std::chrono::nanoseconds;
-// using std::chrono::duration_cast;
 using namespace std;
+using namespace std::chrono;
 
-// void convert_string_to_numbers(string &s, vector<unsigned long> &v, unsigned long &base)
-// {
-//     // every position in vector v will hold a word (4 bytes) - 4 characters worth information
-//     unsigned long word = 0;
-//     for(int i=0; i<s.length(); i+=4, word = 0)
-//     {
-//         for(int j=0, val; j<4; j++)
-//         {
-//             if(i+j >= s.length())
-//                 val = 0;
-//             else
-//                 val = (int)s[i+j];
-//             word = word * base + val;
-//         }
-//     }
-// }
+void convert_string_to_numbers(string &s, vector<unsigned long> &v, unsigned long &base)
+{
+    // every position in vector v will hold a word (4 bytes) - 4 characters worth information
+    unsigned long word = 0;
+    for(int i=0; i<s.length(); i+=4, word = 0)
+    {
+        for(int j=0, val; j<4; j++)
+        {
+            if(i+j >= s.length())
+                val = 0;
+            else
+                val = (int)s[i+j];
+            word = word * base + val;
+        }
+    }
+}
 
 int main()
 {
@@ -33,18 +32,41 @@ int main()
 
     string key_string, msg_string;
     cin >> key_string >> msg_string;
-    
-    vector<unsigned long> key(32), msg;
+
+
+    vector<unsigned long> key(32), msg(32);
     unsigned long base = 1 << 8; // The size of an ASCII character
 
-    // Pending completion
+    convert_string_to_numbers(key_string, key, base);
+    convert_string_to_numbers(msg_string, msg, base);
 
+    // MAC computation using MMH
+    auto start_mmh = high_resolution_clock::now();
 
-    // unsigned long long int h_MMH = mmh(key, msg);
+    unsigned long mmh_mac = mmh(key, msg);
+    
+    auto stop_mmh = high_resolution_clock::now();
+    auto mmh_duration = duration_cast<nanoseconds>(stop_mmh - start_mmh);
 
-    // auto t0 = clock::now();
-    // auto t1 = clock::now();
-    // nanoseconds ns = duration_cast<nanoseconds>(t1-t0);
+    // MAC computation using SQH
+    auto start_sqh = high_resolution_clock::now();
+
+    unsigned long sqh_mac = sqh(key, msg);
+
+    auto stop_sqh = high_resolution_clock::now();
+    auto sqh_duration = duration_cast<nanoseconds>(stop_sqh - start_sqh);
+
+    // Printing outputs and results
+    cout << "---------------MMH-MAC---------------\n";
+    cout << "MAC: " << mmh_mac << "\n";
+    cout << "Execution Time: " << mmh_duration.count() << " nanoseconds\n";
+
+    cout << "\n";
+    cout << "---------------SQH-MAC---------------\n";
+    cout << "MAC: " << sqh_mac << "\n";
+    cout << "Execution Time: " << sqh_duration.count() << " nanoseconds\n";
+
+    
 
     return 0;
 }
